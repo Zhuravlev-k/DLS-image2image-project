08fabfd7a4ec725216aad3f0c1976f9961be1616
Kirill Zhuravlev <a24317@yandex.ru>
2024-02-04 21:24:47 +0300

    add weight for genB

--- bot/imports.py
+++ bot/imports.py
@@ -2,5 +2,11 @@ import os
 import sys
 def add_path():
     cd = os.getcwd()
-    pwd = cd + "\\animefication"
+    # pwd = cd + "/animefication" # unix version
+    pwd = cd + "\\animefication" # win version
     sys.path.append(pwd)
+    
+
+if __name__ == "__main__":
+    add_path()
+    print(sys.path)
